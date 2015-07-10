c824abab80db24eee30b4e3a5d579a572eb35023
Ferris Tseng <ferristseng@gmail.com>
2015-07-10 15:43:40 -0400

    added docs

---
+++ implementors/core/default/trait.Default.js
@@ -0,0 +1,10 @@
+(function() {var implementors = {};
+implementors['freqdist'] = ["impl&lt;K, H = <a class='struct' href='http://doc.rust-lang.org/nightly/core/hash/sip/struct.SipHasher.html' title='core::hash::sip::SipHasher'>SipHasher</a>, S = <a class='struct' href='http://doc.rust-lang.org/nightly/std/collections/hash/state/struct.DefaultState.html' title='std::collections::hash::state::DefaultState'>DefaultState</a>&lt;<a class='struct' href='http://doc.rust-lang.org/nightly/core/hash/sip/struct.SipHasher.html' title='core::hash::sip::SipHasher'>SipHasher</a>&gt;&gt; <a class='trait' href='http://doc.rust-lang.org/nightly/core/default/trait.Default.html' title='core::default::Default'>Default</a> for <a class='struct' href='freqdist/struct.FrequencyDistribution.html' title='freqdist::FrequencyDistribution'>FrequencyDistribution</a>&lt;K, S&gt;",];
+
+            if (window.register_implementors) {
+                window.register_implementors(implementors);
+            } else {
+                window.pending_implementors = implementors;
+            }
+        
+})()
