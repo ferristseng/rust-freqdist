97b30faba8d1d86dbce33adf23378bdea7214583
Ferris Tseng <ferristseng@fastmail.fm>
2016-02-03 11:42:13 -0500

    regenerate docs for 0.1.4

--- implementors/core/iter/trait.Iterator.js
+++ implementors/core/iter/trait.Iterator.js
@@ -1,5 +1,5 @@
 (function() {var implementors = {};
-implementors['freqdist'] = ["impl&lt;'a, K: 'a&gt; <a class='trait' href='http://doc.rust-lang.org/nightly/core/iter/trait.Iterator.html' title='core::iter::Iterator'>Iterator</a> for <a class='struct' href='freqdist/struct.NonZeroKeysIter.html' title='freqdist::NonZeroKeysIter'>NonZeroKeysIter</a>&lt;'a, K&gt;",];
+implementors['freqdist'] = ["impl&lt;'a, K: 'a&gt; <a class='trait' href='https://doc.rust-lang.org/nightly/core/iter/trait.Iterator.html' title='core::iter::Iterator'>Iterator</a> for <a class='struct' href='freqdist/struct.NonZeroKeysIter.html' title='freqdist::NonZeroKeysIter'>NonZeroKeysIter</a>&lt;'a, K&gt;",];
 
             if (window.register_implementors) {
                 window.register_implementors(implementors);
