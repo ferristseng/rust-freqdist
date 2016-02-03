fddd823d9480141441d3000058e41c5c7d53bc90
Ferris Tseng <ferristseng@fastmail.fm>
2016-02-03 10:45:05 -0500

    release under dual license for #1

--- src/lib.rs
+++ src/lib.rs
@@ -1,3 +1,10 @@
+// Copyright 2016 rust-freqdist Developers
+//
+// Licensed under the Apache License, Version 2.0, <LICENSE-APACHE or
+// http://apache.org/licenses/LICENSE-2.0> or the MIT license <LICENSE-MIT or
+// http://opensource.org/licenses/MIT>, at your option. This file may not be
+// copied, modified, or distributed except according to those terms.
+
 //! Implementation of a Frequency Distribution in Rust. Keeps track of how many 
 //! times an object appears in a larger context (for example, how many times a 
 //! word appears in a piece of text). The underlying data structure of the 
